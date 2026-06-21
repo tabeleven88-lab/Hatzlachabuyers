53416326b7b033b53161ae323b5b558f9412c860
tabeleven88-lab <tabeleven88@gmail.com>
2026-06-21 09:53:03 +0300

    Delete js directory

--- js/sections/kvitelService.js
+++
@@ -1,7 +0,0 @@
-// /js/sections/kvitel/kvitelService.js
-
-import { addDocument } from "../service/dbService.js";
-
-export async function submitKvitel(data) {
-  return addDocument("kvitelService", data);
-}
